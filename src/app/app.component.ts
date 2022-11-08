bc056c8c0b2a9aae3d7332618a82c0635a2026aa
Netanel Basal <netanel@utila.io>
2022-11-08 10:37:08 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '@ngneat/svg-icon';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [SvgIconComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'svg-demo';
-}
+export class AppComponent {}
